d3b17f632099abdb415fe9e692bdd9684c57e154
aaronjan <aaronjan98@gmail.com>
2020-01-13 12:28:46 -0800

    created App component and TodoForm/List

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class TodoForm extends Component {
+    constructor {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+            <form >
+                <button></button>
+            </form>
+        );
+    }
+}
