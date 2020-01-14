b1e12e840c266f57e83d54c1542f5205e0efb075
aaronjan <aaronjan98@gmail.com>
2020-01-13 17:17:05 -0800

    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
-// you will need a place to store your state in this component.
-// design `App` to be the parent component of your application.
-// this component is going to take care of state, and any change handlers you need to work with your state
-
-const todoData = [
-  {
-    task: 'Organize Garage',
-    id: 1528817077286,
-    completed: false
-  },
-  {
-    task: 'Bake Cookies',
-    id: 1528817084358,
-    completed: false
-  }
-]
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todoDataList: todoData
+      todoDataList: []
     };
   }
 
@@ -54,11 +38,10 @@ class App extends React.Component {
   };
 
   clearCompleted = () => {
-    const newTodoDataList = this.state.todoDataList.filter(item => 
-      item.completed === false
-    );
     this.setState({
-      todoDataList: newTodoDataList
+      todoDataList: this.state.todoDataList.filter(item => 
+        item.completed === false
+      )
     });
   };
   
